62fa3c3839ddb55927d90353c550cf23423a08b7
Amir Hosein karimi <amirhossein.k787878@gmail.com>
2022-12-09 18:18:37 +0000

    Update 6 files

--- src/components/Home.js
+++ src/components/Home.js
@@ -25,7 +25,35 @@ const Home = () => {
             </div>
           </div>
         </Col>
-        <Col lg={6} className="background"></Col>
+        <Col lg={6} className="background">
+          <div className="contain">
+            <div className="time">
+              <span className="block">
+                شنبه تا چهارشنبه<span>10-10</span>
+              </span>
+              <span className="block">
+                جمعه<span>12-5</span>
+              </span>
+              <span className="block">
+                جمعه<span>10-8</span>
+              </span>
+            </div>
+            <div className="social">
+              <div className="box">
+                <img src="../public/phone.png" />
+                <span>09391470427</span>
+              </div>
+              <div className="box">
+                <img src="" />
+                <span>09391470427</span>
+              </div>
+              <div className="box">
+                <img src="" />
+                <span>09391470427</span>
+              </div>
+            </div>
+          </div>
+        </Col>
       </Row>
     </Container>
   );
