6a4f2c37d6138ae5ed883e019d557201270146a5
Amir Hosein karimi <amirhossein.k787878@gmail.com>
2023-01-02 09:20:48 +0000

    Update Datatable.jsx

--- src/components/Dashboard/datatable/Datatable.jsx
+++ src/components/Dashboard/datatable/Datatable.jsx
@@ -2,60 +2,60 @@ import "./datatable.scss";
 import { DataGrid } from "@mui/x-data-grid";
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import {listProductAction} from '../../../actions/productActions'
+import { listProductAction } from "../../../actions/productActions";
 // ...............
-const columns = [
-  { field: "_id", headerName: "ID", width: 70 },
-  {
-    field: "namecar",
-    headerName: "نام محصول",
-    width: 210,
-    renderCell: (params) => {
-      return (
-        <div className="cellWithImg">
-          <img className="cellImg" src={params.row.pic} alt="avatar" />
-          {params.row.namecar}
-        </div>
-      );
-    },
-  },
-  { field: "factory", headerName: "شرکت", width: 100 },
-  {
-    field: "distance",
-    headerName: "کارکرد",
-    type: "number",
-    width: 80,
-  },
-  {
-    field: "full",
-    headerName: "محصول",
-    description: "This column has a value getter and is not sortable.",
-    sortable: false,
-    width: 160,
-    renderCell: (params) => {
-      return (
-        <>
-          <span>{params.row.namecar}</span>
-          <p>{params.row.factory}</p>
-        </>
-      );
-    },
-  },
-  {
-    field: "status",
-    headerName: "وضعیت",
-    // type: "number",
-    width: 80,
-    renderCell: (params) => {
-      return (
-        <span className={` status ${params.row.status}`}>
-          {/* {params.row.status} */}
-          {`${(params.row.status ==='approved') ? 'موجود' : 'ناموجود'}`}
-        </span>
-      );
-    },
-  },
-];
+// const columns = [
+//   { field: "_id", headerName: "ID", width: 70 },
+//   {
+//     field: "namecar",
+//     headerName: "نام محصول",
+//     width: 210,
+//     renderCell: (params) => {
+//       return (
+//         <div className="cellWithImg">
+//           <img className="cellImg" src={params.row.pic} alt="avatar" />
+//           {params.row.namecar}
+//         </div>
+//       );
+//     },
+//   },
+//   { field: "factory", headerName: "شرکت", width: 100 },
+//   {
+//     field: "distance",
+//     headerName: "کارکرد",
+//     type: "number",
+//     width: 80,
+//   },
+//   {
+//     field: "full",
+//     headerName: "محصول",
+//     description: "This column has a value getter and is not sortable.",
+//     sortable: false,
+//     width: 160,
+//     renderCell: (params) => {
+//       return (
+//         <>
+//           <span>{params.row.namecar}</span>
+//           <p>{params.row.factory}</p>
+//         </>
+//       );
+//     },
+//   },
+//   {
+//     field: "status",
+//     headerName: "وضعیت",
+//     // type: "number",
+//     width: 80,
+//     renderCell: (params) => {
+//       return (
+//         <span className={` status ${params.row.status}`}>
+//           {/* {params.row.status} */}
+//           {`${params.row.status === "approved" ? "موجود" : "ناموجود"}`}
+//         </span>
+//       );
+//     },
+//   },
+// ];
 
 // const rows = [
 //   {
@@ -140,6 +140,39 @@ const columns = [
 //     statusEn: "Approved",
 //   },
 // ];
+// ....................................
+const columns = [
+  { field: "id", headerName: "ID", width: 70 },
+  { field: "firstName", headerName: "First name", width: 130 },
+  { field: "lastName", headerName: "Last name", width: 130 },
+  {
+    field: "age",
+    headerName: "Age",
+    type: "number",
+    width: 90,
+  },
+  {
+    field: "fullName",
+    headerName: "Full name",
+    description: "This column has a value getter and is not sortable.",
+    sortable: false,
+    width: 160,
+    valueGetter: (params) =>
+      `${params.row.firstName || ""} ${params.row.lastName || ""}`,
+  },
+];
+
+const rows = [
+  { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
+  { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
+  { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
+  { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
+  { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
+  { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
+  { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
+  { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
+  { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
+];
 
 // ......................................................
 const actionColumn = [
@@ -160,34 +193,33 @@ const actionColumn = [
 //.......................................................
 
 const Datatable = () => {
-    const [datatable,setDataTable] = useState([])
-    const dispatch = useDispatch();
-    
+  const [datatable, setDataTable] = useState([]);
+  const dispatch = useDispatch();
 
-    
-    const getData = async()=>{
-      await const productList = useSelector((state) => state.productList);
-      const { product } = productList;
-      setDataTable(product)
-    }
-    useEffect(() => {
-      dispatch(listProductAction());
-      getData()
-  }, [
-    dispatch,datatable
-  ]);
+  // const productList = useSelector((state) => state.productList);
+  // const { product } = productList;
+  // setDataTable(product);
+
+  // useEffect(() => {
+  //   // dispatch(listProductAction());
+  // }, [dispatch, datatable]);
   return (
     <div className="datatable">
       <div style={{ height: "100%", width: "100%" }}>
-        
-          <DataGrid
+        {/* <DataGrid
           rows={datatable}
           columns={columns.concat(actionColumn)}
           pdistanceSize={9}
           rowsPerPdistanceOptions={[9]}
           checkboxSelection
+        /> */}
+        <DataGrid
+          rows={rows}
+          columns={columns}
+          pageSize={5}
+          rowsPerPageOptions={[5]}
+          checkboxSelection
         />
-       
       </div>
     </div>
   );
